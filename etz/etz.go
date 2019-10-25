cd9ca229ceb365ac6c580f5675683edca1689103
finley0916 <55075717+finley0916@users.noreply.github.com>
2019-10-25 18:20:28 +0800

    Update etz.go

--- etz/etz.go
+++ etz/etz.go
@@ -1,1 +1,29 @@
 
+package etz
+
+import (
+	"github.com/blocktree/ethereum-adapter/ethereum"
+	"github.com/blocktree/openwallet/log"
+)
+
+const (
+	Symbol    = "ETZ"
+)
+
+type WalletManager struct {
+	*ethereum.WalletManager
+}
+
+func NewWalletManager() *WalletManager {
+	wm := WalletManager{}
+	wm.WalletManager = ethereum.NewWalletManager()
+	wm.Config = ethereum.NewConfig(Symbol)
+	wm.Log = log.NewOWLogger(wm.Symbol())
+	return &wm
+}
+
+//FullName 币种全名
+func (wm *WalletManager) FullName() string {
+	return "Ethereum Classic"
+}
+
