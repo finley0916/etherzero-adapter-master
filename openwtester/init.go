dc1c6999fdf0b16a1e4b936faaaebd2eadfe3543
finley0916 <55075717+finley0916@users.noreply.github.com>
2019-10-25 18:21:07 +0800

    Update init.go

--- openwtester/init.go
+++ openwtester/init.go
@@ -1,1 +1,13 @@
+package openwtester
 
+import (
+	"github.com/blocktree/openwallet/log"
+	"github.com/blocktree/openwallet/openw"
+	"github.com/finley0916/etherzero-adapter-master/tree/master/etz"
+)
+
+func init() {
+	//注册钱包管理工具
+	log.Notice("Wallet Manager Load Successfully.")
+	openw.RegAssets(etz.Symbol, etz.NewWalletManager())
+}
